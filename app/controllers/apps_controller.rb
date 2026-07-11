25cdc85d1ed3a2fb8a50224afc2e579eb901a485
Lucas <contact@lucasvanbriemen.nl>
2026-07-11 12:39:04 +0200

    Refactor default primary database kind to "external".

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -17,7 +17,7 @@ class AppsController < ApplicationController
 
     @app = App.new(app_kind: params[:app_kind].presence || "rails",
                    ruby_version: "3.3.8", git_branch: "main",
-                   primary_db_kind: "sqlite")
+                   primary_db_kind: "external")
   end
 
   def create
